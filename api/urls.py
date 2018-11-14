04b991369de03b1441bf686b707e16ad4c391451
ninemilli.song <ninemilli_song@163.com>
2018-11-14 09:05:08 +0800

    添加api doc

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,6 @@ app_name = 'api'
 urlpatterns = [
     path('', views.index, name='index'),
     path('asset-view/<int:user_id>/', views.assetView, name='assetView'),
-    path('userDetail/<pk>/', views.UserDetail.as_view(), name='user-asset'),
+    path('userDetail/<pk>/', views.UserDetail.as_view(), name='user-asset-detail'),
     path('userList/', views.UserList.as_view(), name='users-asset-list')
 ]
