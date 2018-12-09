2f703124597f65c2e0eaf4fd1f2e5eb33532e083
ninemilli.song <ninemilli_song@163.com>
2018-12-09 16:57:58 +0800

    /auth/getUserInfo/接口不处理异常直接异常由rest_framework框架处理

--- api/views.py
+++ api/views.py
@@ -2,20 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from .models import Investor, Asset
-from .serializer import AssetSerializer, InvestorSerializer, TokenSerializer
-from rest_framework import generics, status
-from rest_framework.views import APIView
+from .serializer import AssetSerializer, InvestorSerializer
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.generics import RetrieveUpdateAPIView
-import json
 from rest_framework import mixins, viewsets, permissions
 from rest_framework.decorators import action
-from django.utils.datastructures import MultiValueDictKeyError
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
 from rest_framework_jwt.settings import api_settings
 from rest_framework_jwt.views import ObtainJSONWebToken, VerifyJSONWebToken
 from datetime import datetime
+from rest_framework import exceptions
+from django.utils.translation import ugettext as _
 
 # Create your views here.
 
@@ -173,24 +169,24 @@ class UserInfo(VerifyJSONWebToken):
         # Create the instance of JSONWebTokenAuthentication to do the authentication job
         authentication = JSONWebTokenAuthentication()
 
-        try:
-            '''
-            authentication.authenticate 会抛出异常，所以添加异常捕获
-            '''
-            auth_data = authentication.authenticate(request)
-            if auth_data is None:
-                return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+        # try:
+        '''
+        authentication.authenticate 会抛出异常，所以添加异常捕获
+        '''
+        auth_data = authentication.authenticate(request)
+        if auth_data is None:
+            raise exceptions.NotAuthenticated()
 
-            '''
-            auth_data type is tuple, first is user instance, the second is jwt value.
-            So I serializer the investor attribute of the first argument.
-            '''
-            serializer = self.get_serializer(auth_data[0].investor)
+        '''
+        auth_data type is tuple, first is user instance, the second is jwt value.
+        So I serializer the investor attribute of the first argument.
+        '''
+        serializer = self.get_serializer(auth_data[0].investor)
 
-            '''
-            Response the data
-            '''
-            return Response(serializer.data)
+        '''
+        Response the data
+        '''
+        return Response(serializer.data)
 
-        except BaseException as exc:
-            return Response(exc, status=status.HTTP_400_BAD_REQUEST)
+        # except BaseException as exc:
+        #     return Response(exc, status=status.HTTP_400_BAD_REQUEST)
