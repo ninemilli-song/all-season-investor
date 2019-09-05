0c669637a09164153c130c9a7971c1621bf72b5a
ninemilli.song <ninemilli_song@163.com>
2019-09-05 19:41:05 +0800

    数据优化，本金为期初金额+积累金额，实现年化收益率

--- api/views/fund_list_view.py
+++ api/views/fund_list_view.py
@@ -1,5 +1,5 @@
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-from ..models import Asset, Initial
+from ..models import Asset, Initial, InvestRecord
 from ..serializer import AssetSerializer
 from rest_framework import status
 from rest_framework.response import Response
@@ -36,23 +36,27 @@ class FundListView(APIView):
             # 根据基金id & 用户 获取基金档案数据
             try:
                 fund = Asset.objects.get(type=initial.fund.id, owner=owner.id)
-                # 现值 - 成本 = 收率
-                principal = fund.principal
-                if principal == 0:
-                    principal = initial.start_amount
-                profit = fund.pv - principal
-                # 利润 / 成本 = 收益率
-                profit_rate = profit / principal
+                # 本金 = 定投期初金额 + 定投累积金额
+                invest_records = InvestRecord.objects.filter(fund=initial.fund.id, owner=owner.id)
+                acc_amount = 0
+                for invest_record in invest_records:
+                    acc_amount += invest_record.amount
+                principal = initial.start_amount + acc_amount
+                # 收率 = 现值 - 成本
+                profit = float('%.2f' % (fund.pv - principal))
+                # 收益率 = 利润 / 成本
+                profit_rate = float('%.4f' % (profit / principal))
                 # 投资时长（秒） = 当前时间（秒） - 定投开始时间（秒）
                 delta_time = datetime.now().timestamp() - initial.start_time.timestamp()
                 # 投资天数 = 投资时长（秒）/ (24 * 60 * 60)
-                delta_days = delta_time / (24 * 60 * 60)
+                delta_days = int(delta_time / (24 * 60 * 60))
+                print('👉🏻 ---> delta_days is: ', delta_days)
                 # 年化收益 = 收益率 / (投资天数 / 365)
-                profit_rate_annual = profit_rate / (delta_days / 365)
+                profit_rate_annual = float('%.4f' % (profit_rate / (delta_days / 365)))
 
                 result.append({
                     'fund': AssetSerializer(fund).data,
-                    'principal': fund.principal,
+                    'principal': principal,
                     'pv': fund.pv,
                     'profit': profit,
                     'profitRate': profit_rate,
