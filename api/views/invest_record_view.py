cb29201cd3f9427e99be9bfe942bb92c168019a3
ninemilli.song <ninemilli_song@163.com>
2019-09-25 15:58:18 +0800

    修改list接口，添加收益率和年化收益率字段‘’

--- api/views/invest_record_view.py
+++ api/views/invest_record_view.py
@@ -1,5 +1,5 @@
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-from ..models import InvestRecord
+from ..models import InvestRecord, Initial
 from ..serializer import InvestRecordSerializer
 from rest_framework import status
 from rest_framework.response import Response
@@ -70,4 +70,32 @@ class InvestRecordView(
             return self.get_paginated_response(serializer.data)
 
         serializer = self.get_serializer(queryset, many=True)
+
+        # 本基金的期初数据
+        fund_initial = Initial.objects.get(fund=fund_id)
+        fund_initial_start_amount = fund_initial.start_amount
+        fund_initial_start_time = fund_initial.start_time
+
+        for item in serializer.data:
+            # 计算到当前定投记录时间为止的定投成本
+            cur_principal = fund_initial_start_amount
+            for i in serializer.data:
+                # 将定投时间下于当前定投记录时间的投入金额记入成本中
+                if float(item.get('date_time')) >= float(i.get('date_time')):
+                    cur_principal += i.get('amount')
+            # 计算当前定投记录时间点下的收益 profit
+            cur_profit = float('%.2f' % (item.get('pv') - cur_principal))
+            # 计算当前定投记录时间眯下的收益率 profit_rate
+            cur_profit_rate = float('%.4f' % (cur_profit / cur_principal))
+            # 计算当前定投记录时间点下的年化收益率 profit_rate_annual
+            delta_time = float(item.get('date_time')) - fund_initial_start_time.timestamp()
+            delta_days = int(delta_time / (24 * 60 * 60))
+            # 年化收益 = 收益率 / (投资天数 / 365)
+            profit_rate_annual = float('%.4f' % (cur_profit_rate / (delta_days / 365)))
+
+            # 补充数据
+            item['cur_profit'] = cur_profit
+            item['cur_profit_rate'] = cur_profit_rate
+            item['profit_rate_annual'] = profit_rate_annual
+
         return Response(serializer.data)
